db5a2e93671a3c4b30101fffb4b54780c6068a64
Ayub <ayyub.amini@gmail.com>
2021-06-26 22:12:02 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FindSmallestPositiveInterger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
